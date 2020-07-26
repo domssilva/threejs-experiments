411469902d55238bf5a7eb2cb1b2d04a647e91b7
domssilva <silva@programmer.net>
2020-07-26 13:58:35 +0200

    add lights to the scene

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,32 +1,48 @@
-import * as THREE from 'three'; // load three.js
+//import THREE lib
+import * as THREE from 'three';
 
 function main() {
-    // select canvas
     const canvas = document.getElementById('canvas');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
 
-    // main three.js object
-    const renderer = new THREE.WebGLRenderer({canvas}); 
-
-    // you pass a scene and a camera to a renderer
+    // 3 essential objects: renderer, scene and camera
+    const renderer = new THREE.WebGLRenderer({canvas});
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+    camera.position.z = 5;
 
-    // creating a mesh (parameters: geometry, material)
+    // mesh construction
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
-    const cube = new THREE.Mesh(geometry, material);
+    const material = new THREE.MeshPhongMaterial({color: 0x44aa88}); // this material is affected by lights!
+    const cube = new THREE.Mesh( geometry, material );
+    
+    // creating a light
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    
+    // add everything to the scene
+    scene.add( camera );
+    scene.add( cube );
+    scene.add(light);
+    
+    // render in full native resolution
+    renderer.setPixelRatio(window.devicePixelRatio);
 
-    // add mesh to your scene
-    scene.add(cube);
-
-    // edit camera position
-    camera.position.z = 2;
-
-    // finally, tell renderer to render everything
-    renderer.render(scene, camera);
+    function render(time) {
+        time *= 0.001; // convert time to seconds
+    
+        cube.rotation.x = time;
+        cube.rotation.y = time;
+    
+        renderer.render(scene, camera);
+    
+        // Request to the browser that you want to animate something
+        requestAnimationFrame(render);
+    }
+    
+    requestAnimationFrame(render);
+    
 }
 
-console.log(PerspectiveCamera);
 main();
