67dbba9c6f28c0961b7566300fcada26ab796402
domssilva <silva@programmer.net>
2020-08-05 09:30:11 +0200

    set orbit control

---
+++ importModel/src/index.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls';
+
+// main settings
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+const canvas = document.body.appendChild( renderer.domElement );
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.update();
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+
+// positioning
+controls.target.set(0, 0, 0);
+camera.position.z = 5;
+
+// add items to scene
+scene.add( cube );
+
+const renderLoop = () => {
+    requestAnimationFrame( renderLoop );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+renderLoop();
