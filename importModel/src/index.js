8dc4487ff8829c5d1c3a1d60173cab63414b713f
domssilva <silva@programmer.net>
2020-08-05 10:54:57 +0200

    import obj mesh

--- importModel/src/index.js
+++ importModel/src/index.js
@@ -1,34 +1,35 @@
 import * as THREE from 'three';
 import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls';
+import {OBJLoader2} from '../node_modules/three/examples/jsm/loaders/OBJLoader2';
 
 // main settings
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 const canvas = document.body.appendChild( renderer.domElement );
 
 // controls
 const controls = new OrbitControls(camera, canvas);
+controls.enableZoom = true;
+controls.enableDamping = true;
+controls.campingFactor = 0.25;
 controls.update();
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
 
 // positioning
 controls.target.set(0, 0, 0);
 camera.position.z = 5;
 
-// add items to scene
-scene.add( cube );
+// object loader
+const assetsPath = './models';         // specify asset's location to find .obj, .mtl, etc...
+const objLoader = new OBJLoader2();
+objLoader.load(`${assetsPath}/car.obj`, (model) => {
+    scene.add(model);
+})
 
 const renderLoop = () => {
     requestAnimationFrame( renderLoop );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 
     renderer.render( scene, camera );
 };
