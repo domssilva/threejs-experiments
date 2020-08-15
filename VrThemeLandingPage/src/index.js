f1a648d6d25b1a5bf5d38679d977360f063ec52d
domssilva <silva@programmer.net>
2020-08-15 13:53:31 +0200

    polish wave motion

--- VrThemeLandingPage/src/index.js
+++ VrThemeLandingPage/src/index.js
@@ -7,7 +7,6 @@ let scene,
     renderer, 
     canvas,
     plane,
-    planeBuffer,
     controls;
 
 init();
@@ -18,9 +17,6 @@ function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     
-    camera.position.z = 15; 
-    camera.position.y = 2;
-
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -33,12 +29,12 @@ function init() {
     controls.enableZoom = true;
     controls.enableDamping = true;
     controls.campingFactor = 0.25;
-    controls.update();
+    controls.autoRotate = true;
 
     // positioning
     controls.target.set(0, 0, 0);
-    camera.position.set(0, 0, 10);
-    
+    camera.position.set(-1.5, -1.6, 18.32);
+
     // lights
     const light	= new THREE.AmbientLight( 0x202020 );
     const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
@@ -51,27 +47,29 @@ function init() {
     const assetsPath = './models';
     objLoader.load(`${assetsPath}/VR_headset.obj`, (model) => {
         model.position.set(0, 0, 0);
-        model.rotateY(70);
+        model.scale.set(2, 2, 2);
         scene.add(model);
     });
 
     // plane
-    const geometry = new THREE.PlaneGeometry( 15, 20, 32 );
-    const material = new THREE.MeshPhongMaterial( {color: 0x888888, wireframe: true, wireframeLinecap: 'butt', side: THREE.DoubleSide} );
+    const geometry = new THREE.PlaneGeometry( 50, 50, 10, 5 );
+    const material = new THREE.MeshPhongMaterial( {color: 0xFFFFFF, wireframe: true, wireframeLinecap: 'butt', shininess: 3, side: THREE.DoubleSide} );
     plane = new THREE.Mesh( geometry, material );
+    
     scene.add( plane );
-    plane.rotation.set(0, .4, THREE.Math.degToRad(90));
-    plane.position.set(5, 0, 0);
+    plane.rotation.set(THREE.Math.degToRad(90), 0, 0);
+    plane.position.set(0, -6, 0);
 
-    // Add listener for window resize.
+    // event listeners
     window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('click', getCameraPosition, false);
 }
 
 function renderLoop(time) {
     requestAnimationFrame( renderLoop );
 
-    // wave plane
-    waves(plane, .8, 4);
+    waves( plane, .4, 2);
+    controls.update();
 
     renderer.render( scene, camera );
 };
@@ -82,34 +80,15 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function wavesBuffer( waveSize, magnitude ) {
-
-    const theTime =  performance.now() * .001;
-    let pos = planeBuffer.geometry.attributes.position;
-  
-    let center = new THREE.Vector3(0,0,0);
-    let vec3 = new THREE.Vector3(); // re-use
-    for ( let i = 0, l = pos.count; i < l; i ++ ) {
-  
-      vec3.fromBufferAttribute(pos, i);
-          vec3.sub(center);
-      
-      let z = Math.sin( vec3.length() /- waveSize + (theTime)) * magnitude;
-      
-      pos.setZ(i, z);
-  
-    }
-  
-    pos.needsUpdate = true
-  
+function getCameraPosition() {
+    console.log('camera position:', camera.position);
+    console.log('camera rotation:', camera.rotation);
 }
 
-function waves( plane, waveSize, magnitude ){
+function waves(plane, waveSize, magnitude) {
 
-const theTime =  performance.now() * .001 ;
-
+    const theTime =  performance.now() * .001 ;
     const center = new THREE.Vector2(0,0);
-
     const vLength = plane.geometry.vertices.length;
 
     for (let i = 0; i < vLength; i++) {
