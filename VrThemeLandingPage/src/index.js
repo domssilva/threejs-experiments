260805d1e156247a26dfc668ea68b687bdcc3019
domssilva <silva@programmer.net>
2020-08-15 11:25:46 +0200

    set wavy wireframed plane

--- VrThemeLandingPage/src/index.js
+++ VrThemeLandingPage/src/index.js
@@ -1,48 +1,124 @@
 import * as THREE from 'three';
 import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls';
 import {OBJLoader2} from '../node_modules/three/examples/jsm/loaders/OBJLoader2';
-import {MTLLoader} from '../node_modules/three/examples/jsm/loaders/MTLLoader';
-import {MtlObjBridge} from '../node_modules/three/examples/jsm/loaders/obj2/bridge/MtlObjBridge';
 
-// main settings
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let scene, 
+    camera,
+    renderer, 
+    canvas,
+    plane,
+    planeBuffer,
+    controls;
 
-camera.position.z = 15; 
-camera.position.y = 2;
+init();
+renderLoop();
 
-const renderer = new THREE.WebGLRenderer({alpha: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-const canvas = document.body.appendChild( renderer.domElement );
+function init() {
+    // main settings
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    camera.position.z = 15; 
+    camera.position.y = 2;
 
-// controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableZoom = true;
-controls.enableDamping = true;
-controls.campingFactor = 0.25;
-controls.update();
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.shadowMap.enabled = true;
 
-// positioning
-controls.target.set(0, 0, 0);
-camera.position.set(0, 0, 10);
+    canvas = document.body.appendChild( renderer.domElement );
 
-// object, mtl loader
-const mtlLoader = new MTLLoader();
-const objLoader = new OBJLoader2();
-const assetsPath = './models';
+    // controls
+    controls = new OrbitControls(camera, canvas);
+    controls.enableZoom = true;
+    controls.enableDamping = true;
+    controls.campingFactor = 0.25;
+    controls.update();
 
-// Or create a three.js material
-objLoader.load(`${assetsPath}/VR_headset.obj`, (model) => {
-    model.position.set(0, 0, 0);
-    model.rotateY(70);
-    scene.add(model);
-});
+    // positioning
+    controls.target.set(0, 0, 0);
+    camera.position.set(0, 0, 10);
+    
+    // lights
+    const light	= new THREE.AmbientLight( 0x202020 );
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    directionalLight.position.set(1, 1, 1).normalize();
+    scene.add(directionalLight);
+    scene.add( light );
+    
+    // object, mtl loader
+    const objLoader = new OBJLoader2();
+    const assetsPath = './models';
+    objLoader.load(`${assetsPath}/VR_headset.obj`, (model) => {
+        model.position.set(0, 0, 0);
+        model.rotateY(70);
+        scene.add(model);
+    });
 
-const renderLoop = () => {
+    // plane
+    const geometry = new THREE.PlaneGeometry( 15, 20, 32 );
+    const material = new THREE.MeshPhongMaterial( {color: 0x888888, wireframe: true, wireframeLinecap: 'butt', side: THREE.DoubleSide} );
+    plane = new THREE.Mesh( geometry, material );
+    scene.add( plane );
+    plane.rotation.set(0, .4, THREE.Math.degToRad(90));
+    plane.position.set(5, 0, 0);
+
+    // Add listener for window resize.
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function renderLoop(time) {
     requestAnimationFrame( renderLoop );
 
+    // wave plane
+    waves(plane, .8, 4);
 
     renderer.render( scene, camera );
 };
 
-renderLoop();
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function wavesBuffer( waveSize, magnitude ) {
+
+    const theTime =  performance.now() * .001;
+    let pos = planeBuffer.geometry.attributes.position;
+  
+    let center = new THREE.Vector3(0,0,0);
+    let vec3 = new THREE.Vector3(); // re-use
+    for ( let i = 0, l = pos.count; i < l; i ++ ) {
+  
+      vec3.fromBufferAttribute(pos, i);
+          vec3.sub(center);
+      
+      let z = Math.sin( vec3.length() /- waveSize + (theTime)) * magnitude;
+      
+      pos.setZ(i, z);
+  
+    }
+  
+    pos.needsUpdate = true
+  
+}
+
+function waves( plane, waveSize, magnitude ){
+
+const theTime =  performance.now() * .001 ;
+
+    const center = new THREE.Vector2(0,0);
+
+    const vLength = plane.geometry.vertices.length;
+
+    for (let i = 0; i < vLength; i++) {
+        let v = plane.geometry.vertices[i];
+        let dist = new THREE.Vector2(v.x, v.y).sub(center);
+
+        v.z = Math.sin(dist.length()/- waveSize + (theTime)) * magnitude;
+    }
+
+    plane.geometry.verticesNeedUpdate = true;
+
+}
